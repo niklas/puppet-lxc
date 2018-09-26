497a0d719339133fb1f08decb7fbe663c2e2f5cc
Niklas Hofer <niklas+dev@lanpartei.de>
2018-09-26 23:08:14 +0200

    puppet v 5

--- manifests/controlling_host.pp
+++ manifests/controlling_host.pp
@@ -1,45 +1,46 @@
-class lxc::controlling_host ($ensure = "present",
-	$provider = "",
+class lxc::controlling_host (
+  $ensure = "present",
+  $provider = "",
   $start_containers = true,
-	$bridge = $lxc::params::bridge) inherits lxc {
+  $bridge = $lxc::params::bridge) inherits lxc {
 
-	package {
-		["lxc", "lvm2", "bridge-utils", "debootstrap", "cgroup-lite"] :
-			ensure => $ensure ;
-	}
-	File {
-		ensure => $ensure,
-		owner => root,
-		group => root,
-	}
-	file{"/etc/default/lxc":
-    content => template('lxc/etc_default_lxc.erb'),
-	}
-		
-	file {
-		['/sys/fs/cgroup'] :
-			ensure => directory ;
+    package {
+      ["lxc", "lvm2", "bridge-utils", "debootstrap", "cgroup-lite"] :
+        ensure => $ensure ;
+    }
+    File {
+      ensure => $ensure,
+      owner => root,
+      group => root,
+    }
+    file{"/etc/default/lxc":
+      content => template('lxc/etc_default_lxc.erb'),
+    }
+    
+    file {
+      ['/sys/fs/cgroup'] :
+        ensure => directory ;
 
-		'/etc/sysctl.d/ipv4_forward.conf' :
-			source => "puppet:///modules/lxc/etc/sysctl.conf",
-			mode => 444 ;
+      '/etc/sysctl.d/ipv4_forward.conf' :
+        source => "puppet:///modules/lxc/etc/sysctl.conf",
+        mode => "444" ;
 
-		'/usr/local/bin/build_vm' :
-			content => template("lxc/build_vm.erb"),
-			mode => 555 ;
-	}
+      '/usr/local/bin/build_vm' :
+        content => template("lxc/build_vm.erb"),
+        mode => "555" ;
+    }
 
 
-        file_line {
-                'enable cgroup memory':
-                        line   => 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT cgroup_enable=memory"',
-                        path   => '/etc/default/grub';
-        }
-	exec {
-		"/usr/sbin/update-grub" :
-			command => "/usr/sbin/update-grub",
-			refreshonly => true,
-			subscribe => File_line["enable cgroup memory"] ;
-	}
-}
+    file_line {
+      'enable cgroup memory':
+        line   => 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT cgroup_enable=memory"',
+        path   => '/etc/default/grub';
+    }
+    exec {
+      "/usr/sbin/update-grub" :
+        command => "/usr/sbin/update-grub",
+        refreshonly => true,
+        subscribe => File_line["enable cgroup memory"] ;
+    }
+  }
 
--- manifests/vm.pp
+++ manifests/vm.pp
@@ -14,103 +14,103 @@ define lxc::vm (
   $arch            = "${architecture}",
   $release         = "${lsbdistcodename}",
   $autostart       = true) {
-  require 'lxc::controlling_host'
+    require 'lxc::controlling_host'
 
-  File {
-    ensure => $ensure, }
-  $c_path = "${container_root}/${name}"
-  $h_name = $name
-  $mac_r = $mac ? {
-    ''      => lxc_genmac($h_name),
-    default => $mac
-  }
-
-  file {
-    "${c_path}/preseed.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      require => Exec["create ${h_name} container"],
-      content => template("lxc/preseed.cfg.erb");
-  }
-
-  if $ip != "manual" {
-    file { "${c_path}/rootfs/etc/network/interfaces":
-      owner     => "root",
-      group     => "root",
-      mode      => 0644,
-      require   => Exec["create ${h_name} container"],
-      subscribe => Exec["create ${h_name} container"],
-      content   => template("lxc/interface.erb");
-    }
-  }
-
-  if defined(Class["dnsmasq"]) {
-    dnsmasq::dhcp-host { "${h_name}-${mac_r}":
-      hostname => $name,
-      mac      => $mac_r,
-    }
-  }
-
-  # FIXME unused - better install backes through puppet?
-  if $addpackages != '' {
-    $addpkg = "-a ${addpackages}"
-  }
-
-  if $ensure == "present" {
-    exec { "create ${h_name} container":
-      command     => "/usr/bin/lxc-create -n ${h_name} -t download -- --dist ${distrib} --release ${release} --arch ${arch}",
-      refreshonly => false,
-      creates     => "${c_path}/config",
-      require     => [ Package['lxc'] ],
-      timeout     => 1200,
-      logoutput   => true,
+    File {
+      ensure => $ensure, }
+    $c_path = "${container_root}/${name}"
+    $h_name = $name
+    $mac_r = $mac ? {
+      ''      => lxc_genmac($h_name),
+      default => $mac
     }
 
-    Replace {
-      require => Exec["create ${h_name} container"], }
-
-    line {
-      "send host-name \"${h_name}\";":
-        line => "send host-name \"${h_name}\";",
+    file {
+      "${c_path}/preseed.cfg":
+        owner   => "root",
+        group   => "root",
+        mode    => "0644",
         require => Exec["create ${h_name} container"],
-        file => "${c_path}/rootfs/etc/dhcp/dhclient.conf";
+        content => template("lxc/preseed.cfg.erb");
     }
 
-    if $passwd != '' {
-      # # setting the root-pw
-      # echo "root:root" | chroot $rootfs chpasswd
-      exec { "set_rootpw: ${h_name}":
-        command     => "echo \"root:${passwd}\" | chroot ${c_path}/rootfs chpasswd",
-        refreshonly => true,
-        require     => Exec["create ${h_name} container"],
-        subscribe   => Exec["create ${h_name} container"],
+    if $ip != "manual" {
+      file { "${c_path}/rootfs/etc/network/interfaces":
+        owner     => "root",
+        group     => "root",
+        mode      => "0644",
+        require   => Exec["create ${h_name} container"],
+        subscribe => Exec["create ${h_name} container"],
+        content   => template("lxc/interface.erb");
       }
     }
 
-    # # Disable root - login via ssh
-    replace { "sshd_noRootlogin: ${h_name}":
-      file        => "${c_path}/rootfs/etc/ssh/sshd_config",
-      pattern     => "PermitRootLogin yes",
-      replacement => "PermitRootLogin no",
-    }
-
-    if $autostart {
-      exec { "/usr/bin/lxc-start -n ${h_name} -d":
-        onlyif  => "/usr/bin/lxc-info -n ${h_name} 2>/dev/null | grep -q STOPPED",
-        require => [Exec["create ${h_name} container"], Exec["${h_name}::install-puppet"]],
+    if defined(Class["dnsmasq"]) {
+      dnsmasq::dhcp-host { "${h_name}-${mac_r}":
+        hostname => $name,
+        mac      => $mac_r,
       }
     }
-  } # end ensure=present
 
-
-
-  file { "/etc/lxc/auto/${h_name}.conf":
-    target => "/var/lib/lxc/${h_name}/config",
-    ensure => $autorun ? {
-      true  => "present",
-      false => "absent",
+    # FIXME unused - better install backes through puppet?
+    if $addpackages != '' {
+      $addpkg = "-a ${addpackages}"
     }
+
+    if $ensure == "present" {
+      exec { "create ${h_name} container":
+        command     => "/usr/bin/lxc-create -n ${h_name} -t download -- --dist ${distrib} --release ${release} --arch ${arch}",
+        refreshonly => false,
+        creates     => "${c_path}/config",
+        require     => [ Package['lxc'] ],
+        timeout     => 1200,
+        logoutput   => true,
+      }
+
+      Replace {
+        require => Exec["create ${h_name} container"], }
+
+        line {
+          "send host-name \"${h_name}\";":
+            line => "send host-name \"${h_name}\";",
+            require => Exec["create ${h_name} container"],
+            file => "${c_path}/rootfs/etc/dhcp/dhclient.conf";
+        }
+
+        if $passwd != '' {
+          # # setting the root-pw
+          # echo "root:root" | chroot $rootfs chpasswd
+          exec { "set_rootpw: ${h_name}":
+            command     => "echo \"root:${passwd}\" | chroot ${c_path}/rootfs chpasswd",
+            refreshonly => true,
+            require     => Exec["create ${h_name} container"],
+            subscribe   => Exec["create ${h_name} container"],
+          }
+        }
+
+        # # Disable root - login via ssh
+        replace { "sshd_noRootlogin: ${h_name}":
+          file        => "${c_path}/rootfs/etc/ssh/sshd_config",
+          pattern     => "PermitRootLogin yes",
+          replacement => "PermitRootLogin no",
+        }
+
+        if $autostart {
+          exec { "/usr/bin/lxc-start -n ${h_name} -d":
+            onlyif  => "/usr/bin/lxc-info -n ${h_name} 2>/dev/null | grep -q STOPPED",
+            require => [Exec["create ${h_name} container"], Exec["${h_name}::install-puppet"]],
+          }
+        }
+    } # end ensure=present
+
+
+
+        file { "/etc/lxc/auto/${h_name}.conf":
+          target => "/var/lib/lxc/${h_name}/config",
+          ensure => $autorun ? {
+            true  => "present",
+            false => "absent",
+          }
+        }
   }
-}
 
